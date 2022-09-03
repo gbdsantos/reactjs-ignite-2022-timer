d11795434099ebe039bd4bdd4bdfb7797ee3cc8c
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-09-02 22:37:50 -0300

    📝 update(frontend): routes layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,15 @@ import { Routes, Route } from 'react-router-dom'
 
 import { Home } from './pages/Home'
 import { History } from './pages/History'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route element={<Home />} path="/" />
-      <Route element={<History />} path="/history" />
+      <Route element={<DefaultLayout />} path="/">
+        <Route element={<Home />} path="/" />
+        <Route element={<History />} path="/history" />
+      </Route>
     </Routes>
   )
 }
