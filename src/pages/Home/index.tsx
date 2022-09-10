c0000a3eeb5d32bd1f3d2f00ea56e6b8401b1ab4
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-09-10 00:18:52 -0300

    📝 update(frontend): enhance inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,14 +18,25 @@ export function Home() {
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
+            list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
             type="text"
           />
 
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Manga" />
+          </datalist>
+
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             id="minutesAmount"
+            max={60}
+            min={5}
             placeholder="00"
+            step={5}
             type="number"
           />
 
